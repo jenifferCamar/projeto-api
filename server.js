dfe6e4812a5a38b907935acc10beec628e8e1553
Jeniffer Camargo Oliveira <jeniffercamargooliveira00@gmail.com>
2026-09-16 15:16:31 -0300

    chore: ajustar configuracoes de seguranca do Express

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const TIME_ZONE = 'America/Sao_Paulo';
 
+app.disable('x-powered-by');
+app.set('json spaces', 2);
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
